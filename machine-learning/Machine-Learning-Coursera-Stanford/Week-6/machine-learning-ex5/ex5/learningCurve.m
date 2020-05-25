2ec4ad362a32703a98ce14aecb0a611be5f1332f
SergiOn <onishhenko@gmail.com>
2020-05-25 21:29:08 +0300

    #machine-learning-coursera-stanford # Week 6 # Programming Assignment: Regularized Linear Regression and Bias/Variance # 2 Bias-variance # 2.1 Learning curves (Learning Curve)

--- machine-learning/Machine-Learning-Coursera-Stanford/Week-6/machine-learning-ex5/ex5/learningCurve.m
+++ machine-learning/Machine-Learning-Coursera-Stanford/Week-6/machine-learning-ex5/ex5/learningCurve.m
@@ -53,10 +53,13 @@ error_val   = zeros(m, 1);
 
 % ---------------------- Sample Solution ----------------------
 
+m_val = size(Xval, 1);
 
-
-
-
+for i = 1:m
+    theta = trainLinearReg([ones(i, 1) X(1:i, :)], y(1:i), lambda);
+    error_train(i) = linearRegCostFunction([ones(i, 1) X(1:i, :)], y(1:i), theta, 0);
+    error_val(i) = linearRegCostFunction([ones(m_val, 1) Xval], yval, theta, 0);
+end
 
 
 % -------------------------------------------------------------

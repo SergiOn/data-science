83c4fa5740672b3f9000a5f9cad02274d5ea71cd
SergiOn <onishhenko@gmail.com>
2020-06-14 21:48:25 +0300

    #machine-learning-coursera-stanford # Week 7 # Programming Assignment: Support Vector Machines # 1 Support Vector Machines # 1.2 SVM with Gaussian Kernels # 1.2.3 Example Dataset 3 (Parameters (C, sigma) for Dataset 3)

--- machine-learning/Machine-Learning-Coursera-Stanford/Week-7/machine-learning-ex6/ex6/dataset3Params.m
+++ machine-learning/Machine-Learning-Coursera-Stanford/Week-7/machine-learning-ex6/ex6/dataset3Params.m
@@ -8,8 +8,8 @@ function [C, sigma] = dataset3Params(X, y, Xval, yval)
 %
 
 % You need to return the following variables correctly.
-C = 1;
-sigma = 0.3;
+% C = 1;
+% sigma = 0.3;
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Fill in this function to return the optimal C and sigma
@@ -23,10 +23,33 @@ sigma = 0.3;
 %        mean(double(predictions ~= yval))
 %
 
+values = [0.01; 0.03; 0.1; 0.3; 1; 3; 10; 30];
 
+s = size(values);
+rows = s(1);
+cols = s(2) + 2;
+results = zeros(rows.^2, cols);
 
+for i = 1:rows % C
+    for j = 1:rows % sigma
 
+        C_temp = values(i);
+        sigma_temp = values(j);
+        index = (i - 1) * rows + j;
+        
+        model = svmTrain(X, y, C_temp, @(x1, x2) gaussianKernel(x1, x2, sigma_temp));
+        predictions = svmPredict(model, Xval);
+        prediction_error = mean(double(predictions ~= yval));
 
+        results(index, :) = [C_temp sigma_temp prediction_error];
+    end
+end
+
+% sort by 3rd column (prediction_error)
+results_sorted = sortrows(results, 3);
+
+C = results_sorted(1, 1);
+sigma = results_sorted(1, 2);
 
 
 % =========================================================================
